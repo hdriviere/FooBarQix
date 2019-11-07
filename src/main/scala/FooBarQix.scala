84f0df9ff2c6f581642cf4ce67de4e6605b21714
Hugo Drivière <hugo.driviere@gmail.com>
2019-11-07 14:47:22 +0100

    My version of FooBarQix

---
+++ src/main/scala/FooBarQix.scala
@@ -0,0 +1,36 @@
+object FooBarQix {
+  def isDivisibleBy(value: Int, divisor: Int): Boolean = { value % divisor == 0 }
+
+  def containsValue(value: Int, dict: Map[Int, String]): Boolean = { value.toString.map((c: Char) => c.asDigit).exists(dict.contains) }
+
+  def compute(value: Int): String = {
+    val dict: Map[Int, String] = Map(3 -> "Foo", 5 -> "Bar", 7 -> "Qix")
+
+    val divisiblePart: List[String] = dict.keys
+      .filter(isDivisibleBy(value, _))
+      .flatMap(dict.get)
+      .toList
+
+    val digits = value.toString.map((c: Char) => c.asDigit)
+
+    val digitsPart: List[String] = if(!containsValue(value, dict) && divisiblePart.isEmpty){
+      digits.map {
+          case 0 => "*"
+          case c => c.toString
+        }
+        .toList
+    } else {
+      digits.map {
+          case 0 => "*"
+          case c => dict.getOrElse(c, "")
+        }
+        .toList
+    }
+
+    divisiblePart.mkString("") + digitsPart.mkString("")
+  }
+
+  def main(args:Array[String]){
+    LazyList.from(1).map(compute).take(200).foreach(println)
+  }
+}
---
+++ src/test/scala/FooBarQixTest.scala
@@ -0,0 +1,35 @@
+import org.scalatest.FunSuite
+
+class FooBarQixTest extends FunSuite {
+  test("Normal case") {
+    assert(FooBarQix.compute(1) == "1")
+  }
+
+  test("Should return Foo when divisible by 3") {
+    assert(FooBarQix.compute(6) == "Foo")
+  }
+
+  test("Should return Bar when divisible by 5") {
+    assert(FooBarQix.compute(10) == "Bar*")
+  }
+
+  test("Should return Qix when divisible by 7") {
+    assert(FooBarQix.compute(14) == "Qix")
+  }
+
+  test("Should return FooFoo when 3") {
+    assert(FooBarQix.compute(3) == "FooFoo")
+  }
+
+  test("Should return FooBarQix when 63571") {
+    assert(FooBarQix.compute(63571) == "FooBarQix")
+  }
+
+  test("Should return FooBarBar when 15") {
+    assert(FooBarQix.compute(15) == "FooBarBar")
+  }
+
+  test("Should return 1*1 when 101") {
+    assert(FooBarQix.compute(101) == "1*1")
+  }
+}
